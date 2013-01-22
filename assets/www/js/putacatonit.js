c6c563b61aa1811bcfb06f555a08fa418416b815
Drake Emko <drakee@gmail.com>
2013-01-22 00:08:25 -0700

    added super ridonkulous basic put-a-cat-on-it technology

---
+++ assets/www/js/putacatonit.js
@@ -0,0 +1,11 @@
+function putCat() {
+	$("#overlay").show();
+	$("#put").hide();
+	$("#remove").show();
+}
+
+function removeCat() {
+	$("#overlay").hide();
+	$("#remove").hide();
+	$("#put").show();
+}
