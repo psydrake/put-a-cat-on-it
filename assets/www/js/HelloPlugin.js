db984e2a212715e95017e23cb70de2e16abb3ee3
Drake Emko <drake@skuloop.com>
2013-01-27 12:30:18 -0700

    removed example plugin

--- assets/www/js/HelloPlugin.js
+++
@@ -1,5 +0,0 @@
-var HelloPlugin = {
-	callNativeFunction: function(success, fail, resultType) {
-		return cordova.exec(success, fail, 'net.edrake.putacatonit.HelloPlugin', 'nativeAction', [resultType]);		
-	}
-}
